1ab4583919b110eabb732980d343cca1b49e5567
SanUtn <sandra.leiva@alumnos.frgp.utn.edu.ar>
2022-10-14 12:08:56 -0300

    se muestra carrito de productos

--- tp-carrito-Leiva-Machado/TPCarrito/Default.aspx.cs
+++ tp-carrito-Leiva-Machado/TPCarrito/Default.aspx.cs
@@ -13,7 +13,7 @@ namespace TPCarrito
     public partial class Default : System.Web.UI.Page
     {
         public List<Articulo> listaArticulos { get; set; }
-        public List<Articulo> listaCarrito { get; set; }
+        
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -32,23 +32,23 @@ namespace TPCarrito
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
             Articulo obj = new Articulo();
+            List<Articulo> listaCarrito = new List<Articulo>();
+                
+                String capturarValor = ((Button)sender).CommandArgument;
             
-            String capturarValor = ((Button)sender).CommandArgument;
-            Console.WriteLine(capturarValor);
             foreach (var art in listaArticulos)
             {
                 if (art.Id == int.Parse(capturarValor))
                 {
                     obj = art;
  
-                    Console.WriteLine(art.Id);
                 }
             }
-
+            listaCarrito.Add(obj);
            
             if (Session["listaArt"] == null)
             {
-                Session.Add("listaArt", obj);
+                Session.Add("listaArt", listaCarrito);
             }
             else
             {
